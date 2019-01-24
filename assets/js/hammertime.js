fc69b9b29e62b7db22e3a31ecc7e65c4b08c800d
Amatson <matti.ojala@iki.fi>
2019-01-24 21:43:10 +0200

    added hammertime-googlyeyes

---
+++ assets/js/hammertime.js
@@ -0,0 +1,33 @@
+$(".move-area").mousemove(function(event) {
+  var eye = $(".eye");
+  var x = (eye.offset().left) + (eye.width() / 2);
+  var y = (eye.offset().top) + (eye.height() / 2);
+  var rad = Math.atan2(event.pageX - x, event.pageY - y);
+  var rot = (rad * (180 / Math.PI) * -1) + 180;
+  eye.css({
+    '-webkit-transform': 'rotate(' + rot + 'deg)',
+    '-moz-transform': 'rotate(' + rot + 'deg)',
+    '-ms-transform': 'rotate(' + rot + 'deg)',
+    'transform': 'rotate(' + rot + 'deg)'
+  });
+});
+
+var caller = $("#escape");
+
+function foo()
+{
+    var randX = Math.floor(Math.random() * (window.innerWidth - 100));
+    var randY = Math.floor(Math.random() * (window.innerHeight - 100));
+    console.log([randX, randY]);
+    caller.stop().animate({"left": randX + "px", "top": randY + "px"});
+
+}
+
+$(document).ready(function() {
+
+    caller.on('mouseenter', foo);
+    caller.on('click', function(){
+        alert('clicked!');
+    });
+
+});
